634eef2246292bebab01f9bb4da56ddec0d22d52
rwangfengme <rwangfengdev@gmail.com>
2016-03-19 22:20:06 -0400

    first commit hello world & change state

---
+++ helloworld.js
@@ -0,0 +1,10 @@
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+ReactDOM.render(
+  <HelloMessage name="John" />,
+  document.getElementById('container')
+);
